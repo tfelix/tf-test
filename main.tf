a605810279038626d0dd0e4ff97d62f188209b70
Thomas Felix <tfelix@meshcloud.io>
2023-08-28 14:48:32 +0200

    Next commit

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
 # main.tf
-terraform {
-  backend "gcs" {
-    bucket = "meshcloud-tf-states"
-    prefix = "auth-test"
+resource "null_resource" "default" {
+  provisioner "local-exec" {
+    command = "echo 'Hello World'"
   }
-}
-
-variable "project_id" {
-  description = "ID of the project"
-}
-
-data "google_project" "project" {
-  project_id = var.project_id
-}
-
-output "project_number" {
-  value = data.google_project.project.number
 }
